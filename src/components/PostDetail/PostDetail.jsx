d501dc1fef18834eaa5ce3a3bf2bf7cdc52058bf
ajmalchy <ajmal.chy.biz@gmail.com>
2023-04-05 18:11:58 -0400

    first commit

---
+++ src/components/PostDetail/PostDetail.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useLoaderData, useNavigate } from 'react-router-dom';
+
+const PostDetail = () => {
+    const post = useLoaderData();
+    console.log(post);
+    const {id, title, useId, body} = post;
+    
+    const ghf = useNavigate();
+    const  handleGoBack = () =>{
+        ghf(-1);
+    }
+    return (
+        <div>
+            <h3>Details about your post of:{id}</h3>
+            <h5>{title}</h5>
+            <p>{body}</p>
+            <button onClick={handleGoBack}>Go back</button>
+
+        </div>
+    );
+};
+
+export default PostDetail;
